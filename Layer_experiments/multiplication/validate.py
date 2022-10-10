87cdb797efbd3225c2a74684e7b1beb6f576482b
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-10-10 11:23:36 +0200

    added intializer

--- Layer_experiments/multiplication/validate.py
+++ Layer_experiments/multiplication/validate.py
@@ -1,18 +1,20 @@
 import onnxruntime as ort
 import numpy as np
 from onnx import load
+from onnx.shape_inference import infer_shapes
 
 # Load in the onnx model
 onnx_model = load("multiplication\onnx_models\multiply_model_split.onnx")
 
 # Set input tensors to random values
-in1 = np.random.random([1,3,512,512]).astype(np.float32)
-b = np.random.random([1]).astype(np.float32)
+# TODO: Would be nice to automate this input tensor
+in1 = np.random.random([1,16,256,256]).astype(np.float32)
+b = np.load("multiplication/b.npy")
 # out_ort = np.random.random([1,16,512,512]).astype(np.float32)
 output = in1*b
 
 # Set the input dict
-input_dict = {"278": in1, "279": b}
+input_dict = {"282": in1, "283": b}
 sess = ort.InferenceSession(onnx_model.SerializeToString())
 out_ort = sess.run(None,input_dict)
 
