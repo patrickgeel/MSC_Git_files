cf444b8a53d9c234e7c9b4c73e46520e205429c9
patrickgeel <p.geel@student.tudelft.n>
2022-10-18 12:24:18 +0200

    Mul_9 build

--- Layer_experiments/splitting/split_run.sh
+++ Layer_experiments/splitting/split_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sn="$1"
-#python splitting_ty.py "$1"
+python splitting_ty_script.py "$1"
 cp /workspace/results/"$1"/*_zynq_proj*/*.runs/impl_1/top_wrapper_utilization_placed.rpt output_final/split_model_"$1"/
