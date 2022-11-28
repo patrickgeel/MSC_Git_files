21cc3255dee9f6405b3eddbfb2e48651349ec714
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-11-28 10:47:17 +0100

    parallelize splitting

--- mobilenet_v1/check_fits.py
+++ mobilenet_v1/check_fits.py
@@ -2,16 +2,19 @@ from src.split_model import split_model
 from src.auto_build import bit_build, estimate_report, fits_kv260
 import os
 import shutil
+from multiprocessing import Process
 from qonnx.core.modelwrapper import ModelWrapper
 
-def main():
+def get_processes():
     model = ModelWrapper("models/mobilenetv1-w4a4_pre_post_tidy.onnx")
     model_dir = "/home/pgeel/bulk/FINNv0.8.1_repo/build_KV260/finn/notebooks/MSC_Git_files/mobilenet_v1/models"
+    
+    processes = []
 
     print(model_dir)
     splits = []
     for n in model.graph.node:
-        if n.op_type == "Mul":
+        if n.op_type == "BatchNormalization":
             splits.append(n.output)
 
     for sn in splits[0::5]:
@@ -29,16 +32,18 @@ def main():
         # Check if estimate fits on KV260, If fits make a bit file
         resource_report = "{}/report/estimate_layer_resources.json".format(final_output_dir)
         if fits_kv260(resource_report):
-            try:    
-                final_output_dir = "build-{}/fit/{}".format("KV260",split_node)
-                if not os.path.exists(final_output_dir):
-                    bit_build(model_file,final_output_dir,folding_config_file,split_node)
-            except:
-                pass
+            processes.append(Process(target=bit_build,args=(model_file,final_output_dir,folding_config_file,split_node,)))
+            # try:    
+            #     final_output_dir = "build-{}/fit/{}".format("KV260",split_node)
+            #     if not os.path.exists(final_output_dir):
+            #         bit_build(model_file,final_output_dir,folding_config_file,split_node)
+            # except:
+            #     pass
         else:
             print("--"*20,"Does not fit", "--"*20)
             print("\t"*20, split_node)
-            return
+            break
+    return processes
 
 if __name__ == "__main__":
-    main()
+    print(get_processes())
