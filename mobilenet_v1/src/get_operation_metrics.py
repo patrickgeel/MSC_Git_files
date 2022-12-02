78b14da67e4b290a170ab759b34a7328328d5ce0
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-12-02 14:01:58 +0100

    update src code for operation metrics

--- mobilenet_v1/src/get_operation_metrics.py
+++ mobilenet_v1/src/get_operation_metrics.py
@@ -22,34 +22,37 @@ def get_op_counts(
     op_dict = dict(zip(operations, counts.astype(str)))
     return op_dict
 
-model_dir = "{}/{}/".format(args.model_dir,args.op_type)
-partion_dir = os.listdir(model_dir)
-metrics_0, metrics_1 = {}, {}
+def main():
+    model_dir = "{}/{}/".format(args.model_dir,args.op_type)
+    partion_dir = os.listdir(model_dir)
+    metrics_0, metrics_1 = {}, {}
 
-for mf in partion_dir:
-    node_split = mf
-    dir = os.path.join(model_dir, mf)
-    if os.path.isdir(dir):
-        # print(dir)
-        files = [os.path.join(dir, f) for f in os.listdir(
-            dir) if (f.endswith("0.onnx") or f.endswith("1.onnx"))]
-        for f in files:
-            partition = f.split("\\")[-1].split('.onnx')[0]
-            ops = get_op_counts(f)
+    for mf in partion_dir:
+        node_split = mf
+        dir = os.path.join(model_dir, mf)
+        if os.path.isdir(dir):
+            # print(dir)
+            files = [os.path.join(dir, f) for f in os.listdir(
+                dir) if (f.endswith("0.onnx") or f.endswith("1.onnx"))]
+            for f in files:
+                partition = f.split("\\")[-1].split('.onnx')[0]
+                ops = get_op_counts(f)
 
-            if partition == "partition_0":
-                metrics_0[node_split] = ops
-            elif partition == "partition_1":
-                metrics_1[node_split] = ops
+                if partition == "partition_0":
+                    metrics_0[node_split] = ops
+                elif partition == "partition_1":
+                    metrics_1[node_split] = ops
+    generate_csv_file([metrics_0,metrics_1])
 
 def generate_csv_file(metrics:list):
     df_0 = pd.DataFrame(metrics[0])
     df_1 = pd.DataFrame(metrics[1])
     # print(model_dir)
-    if not os.path.exists("{}/metrics".format(model_dir)):
-        os.makedirs("{}/metrics".format(model_dir))
-    df_0.to_csv("{}/metrics/op_count_{}_partition_0.csv".format(model_dir, args.op_type))
-    df_1.to_csv("{}/metrics/op_count_{}_partition_1.csv".format(model_dir, args.op_type))
-    print("Wrote data to file: ", "{}/metrics/op_count_{}".format(model_dir,args.op_type))
+    if not os.path.exists("{}/metrics".format(args.model_dir)):
+        os.makedirs("{}/metrics".format(args.model_dir))
+    df_0.to_csv("{}/metrics/op_count_{}_partition_0.csv".format(args.model_dir, args.op_type))
+    df_1.to_csv("{}/metrics/op_count_{}_partition_1.csv".format(args.model_dir, args.op_type))
+    print("Wrote data to file: ", "{}/metrics/op_count_{}".format(args.model_dir,args.op_type))
 
-generate_csv_file([metrics_0,metrics_1])
+if __name__ == "__main__":
+    main()
