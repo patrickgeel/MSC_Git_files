79461fff53d1df47691f5b58ee65645c85818085
ubuntu <p.geel@student.tudelft.nl>
2022-12-15 16:24:39 +0000

    WORKING FULL ORT!

--- mobilenet_v1/test_build/experiment/op_test.py
+++ mobilenet_v1/test_build/experiment/op_test.py
@@ -8,20 +8,6 @@ from onnx.helper import make_attribute
 from driver_base import FINNExampleOverlay
 from driver import io_shape_dict
 
-model = ModelWrapper("dataflow_parent.onnx")
-DOMAIN = "ai.onnx.contrib"
-
-for n in model.graph.node:
-    if n.domain == "finn.custom_op.fpgadataflow":
-        n.domain = DOMAIN
-    if n.op_type == "StreamingDataflowPartition":
-        n.name = ''
-        n.attribute.append(make_attribute("bitfile","finn-accel.bit"))
-        
-# print(model.get_tensor_datatype(model.graph.input[0].name))
-model.save("dataflow_parent_updated.onnx")
-print(io_shape_dict)
-
 platform = "zynq-iodma"
 batch_size = 1
 bitfile = "finn-accel.bit"
@@ -45,4 +31,3 @@ inp = np.load("dog.npy")
 
 model_func = PyOrtFunction.from_model("./dataflow_parent_updated.onnx")
 outputs = model_func(inp)
-# print(outputs)
